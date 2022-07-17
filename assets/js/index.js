9e9c2a686c4aacc298c6b6a496fb90adf7fa7849
Jane <nonstop00066@gmail.com>
2022-07-17 03:14:54 +0300

    Create handleArticle function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const cardsSection = document.getElementById("cards-section");
 const cardsContainer = document.getElementById("cards-container");
 
@@ -31,11 +29,11 @@ function createActorsCard(actor) {
       const socialLink = createElement('a', {classNames: ['fa-stack', 'fa-sm'], attributes: {'href': contact}}, spanCircleSocialLink, spanItemSocialLink );
       return socialLink;
     })
-    const actorsName = createElement('p', {classNames: ['actors-name']}, actorName );
+    const actorsName = createElement('p', {classNames: ['actors-name'], attributes: {'id': id, 'data-id': id}}, actorName );
     const cardPhoto = createElement('img', {classNames: ['card-photo'], attributes:{'src': profilePicture, "alt": actorName, 'data-id': id}, events:{error: handlePhotoError, "load": handlePhotoLoad}} );
-    const initialsWrapper = createElement('div', {classNames: ['initials-wrapper', 'color-secondary'], name: [firstName, lastName]} );
-    const photoWrapper = createElement('div', {classNames: ['photo-wrapper']}, initialsWrapper, cardPhoto );
-    const cardArticle = createElement('article', {classNames: ['card-article']}, photoWrapper, actorsName, ...socials );
+    const initialsWrapper = createElement('div', {classNames: ['initials-wrapper', 'color-secondary'], attributes: {'data-id': id}, name: [firstName, lastName]} );
+    const photoWrapper = createElement('div', {classNames: ['photo-wrapper'], attributes: {'data-id': id}}, initialsWrapper, cardPhoto );
+    const cardArticle = createElement('article', {classNames: ['card-article'], attributes: {'data-id': id}, events: {'click': handleArticle}}, photoWrapper, actorsName, ...socials );
     return cardArticle;
 }
 
@@ -62,10 +60,22 @@ function getInitials(array) {
 
 function handlePhotoError({ target }) {
     target.remove();
-}
+  }
 
 function handlePhotoLoad({ target }) {
     target.classList.add('background-color');
+}
+
+const actorsNames = [];
+
+function handleArticle({ target }) {
+  const actorsName = document.getElementById(target.dataset.id).textContent;
+  if (!actorsNames.includes(actorsName)) {
+    actorsNames.push(actorsName);
+    const actorsNameItem = createElement("li", {classNames: ['actors-name']}, actorsName);
+    const chosenList = document.getElementById('chosen-list');
+    chosenList.append(actorsNameItem); 
+  }
 }
 
 function stringToColour(string) {
@@ -79,4 +89,4 @@ function stringToColour(string) {
       colour += ("00" + value.toString(16)).slice(-2);
     }
     return colour;
-}
+  }
