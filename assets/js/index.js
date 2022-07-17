626d0cb0bba0d54c1b7c9b7e9538cc2cf5d2c848
Jane <nonstop00066@gmail.com>
2022-07-17 03:10:56 +0300

    Create chosenContainer & errorMessage fuctions

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,45 +10,64 @@ fetch('./assets/js/data.json')
             .filter((actor) => actor.firstName && actor.lastName && actor.profilePicture)
             .map((actor) => createActorsCard(actor));
         cardsContainer.append(...HTMLCollectionActors);
+        cardsSection.append(createChosenContainer());
     })
     .catch(() => {
+        cardsContainer.append(createErrorMessage());
     })
 
 function createActorsCard(actor) {
-  const { id, firstName, lastName, profilePicture, contacts } = actor;
-  const actorName = firstName && lastName ? `${firstName} ${lastName}` : "Anonim";
-  const spanItemSocialLink = createElement('span', {classNames: ['color-secondary', 'fa', 'fa-facebook-f', 'fa-stack-1x', 'fa-xs']} );
-  const spanCircleSocialLink = createElement('span', {classNames: ['fa', 'fa-circle', 'fa-stack-2x']} );
-  const socialLink = createElement('a', {classNames: ['fa-stack', 'fa-sm'], attributes: {href: contacts}}, spanCircleSocialLink, spanItemSocialLink );
-  const actorsName = createElement('p', {classNames: ['actors-name'], attributes: {'id': id}}, actorName );
-  const cardPhoto = createElement('img', {classNames: ['card-photo'], attributes:{"src": profilePicture, "alt": actorName}, events:{error: handlePhotoError, "load": handlePhotoLoad}} );
-  const initialsWrapper = createElement('div', {classNames: ['initials-wrapper', 'color-secondary'], name: [firstName, lastName]} );
-  const photoWrapper = createElement('div', {classNames: ['photo-wrapper']}, initialsWrapper, cardPhoto );
-  const cardArticle = createElement('article', {classNames: ['card-article']}, photoWrapper, actorsName, socialLink );
-  return cardArticle;
+    const { id, firstName, lastName, profilePicture, contacts } = actor;
+    const actorName = firstName && lastName ? `${firstName} ${lastName}` : "Anonim";
+    const spanItemSocialLink = createElement('span', {classNames: ['color-secondary', 'fa', 'fa-facebook-f', 'fa-stack-1x', 'fa-xs']} );
+    const spanCircleSocialLink = createElement('span', {classNames: ['fa', 'fa-circle', 'fa-stack-2x']} );
+    const socialLink = createElement('a', {classNames: ['fa-stack', 'fa-sm'], attributes: {href: contacts}}, spanCircleSocialLink, spanItemSocialLink );
+    const actorsName = createElement('p', {classNames: ['actors-name']}, actorName );
+    const cardPhoto = createElement('img', {classNames: ['card-photo'], attributes:{'src': profilePicture, "alt": actorName, 'data-id': id}, events:{error: handlePhotoError, "load": handlePhotoLoad}} );
+    const initialsWrapper = createElement('div', {classNames: ['initials-wrapper', 'color-secondary'], name: [firstName, lastName]} );
+    const photoWrapper = createElement('div', {classNames: ['photo-wrapper']}, initialsWrapper, cardPhoto );
+    const cardArticle = createElement('article', {classNames: ['card-article']}, photoWrapper, actorsName, socialLink );
+    return cardArticle;
+}
+
+function createChosenContainer() {
+  const actorsName = createElement('li', {classNames: ['actors-name']});
+  const chosenList = createElement('ul', {classNames: ['chosen-list'], attributes: {'id': 'chosen-list'}}, actorsName);
+  const heading = createElement('h2', {classNames: ['heading']}, document.createTextNode('you choosed'));
+  const chosenContainer = createElement('div', {classNames: ['chosen-container']}, heading, chosenList );
+  return chosenContainer;
+}
+
+function createErrorMessage() {
+  const errorMessageContent3 = createElement('p', {classNames: ['error-message-content']}, document.createTextNode('We\'ll fix this page soon.'));
+  const errorMessageContent2 = createElement('p', {classNames: ['error-message-content']}, document.createTextNode('Please try again later!'));
+  const errorMessageContent1 = createElement('p', {classNames: ['error-message-content']}, document.createTextNode('Sorry, this page is temporarily unavailable.'));
+  const errorHeading = createElement('h2', {classNames: ['heading', 'error-heading']}, document.createTextNode('Oops! Something went wrong!'));
+  const errorMessage = createElement('div', {classNames: ['error-message-container']}, errorHeading, errorMessageContent1, errorMessageContent2, errorMessageContent3 );
+  return errorMessage;
 }
 
 function getInitials(array) {
-  return array.toString().toUpperCase().split(",").map((string) => string.slice(0, 1)).join("") || 'NN';
+    return array.toString().toUpperCase().split(",").map((string) => string.slice(0, 1)).join("") || 'NN';
 }
 
 function handlePhotoError({ target }) {
-  target.remove();
-}
+    target.remove();
+  }
 
 function handlePhotoLoad({ target }) {
-  target.classList.add('background-color');
+    target.classList.add('background-color');
 }
 
 function stringToColour(string) {
-  let hash = 0;
-  for (let i = 0; i < string.length; i++) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    let hash = 0;
+    for (let i = 0; i < string.length; i++) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = "#";
+    for (let i = 0; i < 3; i++) {
+      const value = (hash >> (i * 8)) & 0xff;
+      colour += ("00" + value.toString(16)).slice(-2);
+    }
+    return colour;
   }
-  let colour = "#";
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff;
-    colour += ("00" + value.toString(16)).slice(-2);
-  }
-  return colour;
-}
